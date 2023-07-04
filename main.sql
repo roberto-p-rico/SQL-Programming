bc21f5b17d80d38dee4180201167f872c62883e3
Roberto Rico <104702856+robertoricop@users.noreply.github.com>
2023-07-04 12:45:59 -0400

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,14 +1,13 @@
--- create a table
 CREATE TABLE student (
   id INTEGER PRIMARY KEY,
   name VARCHAR(20) NOT NULL,
   major VARCHAR(20) NULL
 );
--- insert some values
+
 INSERT INTO student VALUES (1, 'Jack', 'Biology');
 INSERT INTO student VALUES (2, 'Kate', 'Sociology');
 INSERT INTO student VALUES (3, 'Claire', 'Chemistry');
 INSERT INTO student VALUES (4, 'Ryan', 'Economics');
 INSERT INTO student VALUES (5, 'Mike', 'Computer Science');
--- fetch some values
+
 SELECT * FROM student
